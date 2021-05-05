b950dbb7979542ffa8b0785978a1b4c2e2d054cf
klimakaite <paulinaklimakaite@gmail.com>
2021-05-05 13:24:13 +0300

    pridėtas įvykių žurnalas

--- editTask.php
+++ editTask.php
@@ -11,6 +11,9 @@ if (mysqli_connect_errno()) {
 } else {
     if (isset($_POST['submit-task-btn']) && isset($_POST['edit-task-id'])) {
 
+        $row=mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT status FROM tasks WHERE task_ID=".$_POST['edit-task-id'].""));
+        $_SESSION['beforeChange'] =$row['status'];
+
         $sql = "UPDATE tasks set title='" . htmlentities(trim($_POST['edit-task-title-input'])) . "', description='" . htmlentities($_POST['edit-task-description-area']) . "', update_date='" . date("Y-m-d") . "', status = '" . htmlentities($_POST['edit-status-select']) . "', priority = '" . htmlentities($_POST['edit-priority-select']) . "' where task_ID=" . $_POST['edit-task-id'];
 
         $res = mysqli_query($mysqli, $sql);
@@ -21,4 +24,3 @@ if (mysqli_connect_errno()) {
     }
 }
 mysqli_close($mysqli);
-
---
+++ log-journal.php
@@ -0,0 +1,66 @@
+<?php
+
+include 'dbh.php';
+
+if (mysqli_connect_errno()) {
+    printf("Failed to connect to database: ", mysqli_connect_error());
+    exit();
+
+} else {
+
+  if($_SESSION['added']=='yes'){
+
+    $sql = "INSERT INTO log_journal (project, user, event_time, event) VALUES ('".$_SESSION['project-id']."', '".$_SESSION['email']."', now(), 'Project was added')";
+    $res = mysqli_query($mysqli, $sql);
+
+  }
+
+  if($_SESSION['edited']=='yes'){
+
+    $sql = "INSERT INTO log_journal (project, user, event_time, event) VALUES ('".$_POST['edit-id']."', '".$_SESSION['email']."', now(), 'Project was edited')";
+    $res = mysqli_query($mysqli, $sql);
+
+  }
+
+  if($_SESSION['added2']=='yes'){
+
+    $sql = "INSERT INTO log_journal (project, task, user, event_time, event) VALUES ('".$_GET['projectIndex']."', '".$_SESSION['task-id']."', '".$_SESSION['email']."', now(), 'Task was added')";
+    $res = mysqli_query($mysqli, $sql);
+
+  }
+
+  if($_SESSION['editedTask']=='yes'){
+    $afterChange=$_POST['edit-status-select'];
+    $beforeChange=$_SESSION['beforeChange'];
+    if($beforeChange!=$afterChange){
+
+      $row=mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT status FROM statuses WHERE status_ID=".$afterChange.""));
+      $status =$row['status'];
+
+      $sql = "INSERT INTO log_journal (project, task, user, event_time, event) VALUES ('".$_GET['projectIndex']."', '".$_POST['edit-task-id']."', '".$_SESSION['email']."', now(), 'Task was edited: status was changed to ".$status."')";
+      $res = mysqli_query($mysqli, $sql);
+    } else{
+      $sql = "INSERT INTO log_journal (project, task, user, event_time, event) VALUES ('".$_GET['projectIndex']."', '".$_POST['edit-task-id']."', '".$_SESSION['email']."', now(), 'Task was edited')";
+      $res = mysqli_query($mysqli, $sql);
+    }
+  }
+
+  if($_SESSION['deleted']=='yes'){
+
+    $sql = "INSERT INTO log_journal (project, user, event_time, event) VALUES ('".$_POST['delete-id']."', '".$_SESSION['email']."', now(), 'Project was deleted')";
+    $res = mysqli_query($mysqli, $sql);
+
+  }
+
+  if($_SESSION['deletedTask']=='yes'){
+
+    $sql = "INSERT INTO log_journal (project, task, user, event_time, event) VALUES ('".$_GET['projectIndex']."', '".$_POST['delete-id']."', '".$_SESSION['email']."', now(), 'Task was deleted')";
+    $res = mysqli_query($mysqli, $sql);
+
+  }
+
+}
+
+mysqli_close($mysqli);
+
+ ?>
