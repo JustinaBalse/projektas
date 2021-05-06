b2e892ac7f579b2772b6730b47b26bf6e2f6d287
Spacekitsune <70965432+Spacekitsune@users.noreply.github.com>
2021-05-06 19:37:40 +0300

    antra dalis
    Pataisytas css
    pridėtas modalinis langas
    pataisyta password ir email logika

--- register.php
+++ register.php
@@ -13,68 +13,31 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <script src="js/scripts.js"></script>
-
 </head>
 <body>
+<!--Modal page: Successful signup-->
+<div class='modal fadeInDown' id='signupsuccsess-modal' tabindex='1' role='dialog' >
+    <div class='modal-dialog modal-md'>
+        <div class='modal-content p-5'>
+            <p class='d-flex justify-content-center mt-10'>Sign up successful!</p>
+            <i class='fas fa-check fa-5x text-success d-flex justify-content-center'></i>
 
+            <form id='open-back-form' method='post' action='index.php'>
 
+                <div class='d-flex justify-content-center mt-4'>
+                    <a href="index.php" class='btn bg-primary text-white m-1' id='signupsuccsess-btn' data-dismiss='modal'>Back to login page
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-<!--<div class='modal fade bd-add-project-lg' id='open-back-modal' tabindex='-1' role='dialog'-->
-<!--aria-labelledby='myLargeModalLabel' aria-hidden='true' data-keyboard='false' data-backdrop='static'>-->
-<!--<div class='modal-dialog modal-md'>-->
-<!--    <div class='modal-content p-5'>-->
-<!--        <p class='d-flex justify-content-center mt-10'>Sign up successful!</p>-->
-<!--        <i class='fas fa-check fa-5x text-success d-flex justify-content-center'></i>-->
-<!---->
-<!--        <form id='open-back-form' method='post' action='index.php'>-->
-<!---->
-<!--            <div class='d-flex justify-content-center mt-4'>-->
-<!--                <button class='btn bg-primary text-white m-1' id='back-btn' data-dismiss='modal'>Back to login-->
-<!--                </button>-->
-<!--            </div>-->
-<!--        </form>-->
-<!--    </div>-->
-<!--</div>-->
-<!--</div>-->
-<!---->
-<!---->
-<!-- Script from preventing resubmitting edit form, prevents pop up after page refresh.-->
-<!--<script>-->
-<!--    $('#back-btn').click(function() {-->
-<!--        window.location.href = 'index.php';-->
-<!--        return false;-->
-<!--    });-->
-<!--</script>-->
-<!---->
-<?php
-//if ($_SESSION['signedup'] == "yes") {
-//?>
-<!---->
-<!--<script>-->
-<!--    $(function (e) {-->
-<!--        $('#open-back-modal').modal('show');-->
-<!--    });-->
-<!--</script>-->
-<!---->
-<!--    --><?php
-//
-//    $_SESSION['signedup'] = "no";
-//}
-//?>
-    
-
-<div class="wrapper fadeInDown">
+<div class="wrapper fadeInDown" id="signup-form">
   <div id="formContent">
-    <!-- Tabs Titles -->
-
-    <!-- Icon -->
     <div class="fadeIn first img-div">
         <a href="index.php"><img src="images/proact-logo.png" id="icon" alt="ProAct"></a>
     </div>
-
-
-
-    <!-- Login Form -->
     <form action="register.inc.php" method="post">
       <input type="text" id="username" class="fadeIn second" name="username" placeholder="Username">
         <div style="width: 100%; margin: auto; padding-left:30px ">
@@ -86,50 +49,68 @@
       <input type="password" id="pwd-repeat" class="fadeIn third" name="repeatPassword" placeholder="Repeat password">
       <input type="submit" class="fadeIn fourth" value="Sign up" name="register">
     </form>
+      <?php
+      if (isset($_GET['error'])) {
+          if ($_GET['error'] == "emptyfields" ) {
+              echo "<div id='formFooter'><a class='underlineHover'>Fill in all fields!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invaliduidmail") {
+              echo "<div id='formFooter'><a class='underlineHover'>Invalid username and e-mail!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invaliduid") {
+              echo "<div id='formFooter'><a class='underlineHover'>Invalid username!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invalidname") {
+              echo "<div id='formFooter'><a class='underlineHover'>Invalid first name!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invalidsurname") {
+              echo "<div id='formFooter'><a class='underlineHover'>Invalid last name!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invalidmail") {
+              echo "<div id='formFooter'><a class='underlineHover'>Invalid e-mail!</a></div> \n";
+          }
+          else if ($_GET['error'] == "invalidpassword") {
+              echo "<div id='formFooter'><a class='underlineHover'>Password must contain at least one uppercase and one lowercase letter, at least one number and one symbol. Password length 8-30 characters.</a></div> \n";
+          }
+          else if ($_GET['error'] == "passwordcheck") {
+              echo "<div id='formFooter'><a class='underlineHover'>Your passwords do not match!</a></div> \n";
+          }
+          else if ($_GET['error'] == "usertaken") {
+              echo "<div id='formFooter'><a class='underlineHover'>This username is already taken!</a></div> \n";
+          }
+          else if ($_GET['error'] == "mailtaken") {
+              echo "<div id='formFooter'><a class='underlineHover'>This email is already taken!</a></div> \n";
+          }
+      }
+      ?>
   </div>
 
-    <?php
-    if (isset($_GET['error'])) {
-        if ($_GET['error'] == "emptyfields" ) {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Fill in all fields!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invaliduidmail") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Invalid username and e-mail!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invaliduid") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Invalid username!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invalidname") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Invalid first name!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invalidsurname") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Invalid last name!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invalidmail") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Invalid e-mail!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "invalidpassword") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Password must contain at least one uppercase and one lowercase letter, at least one number and one symbol. Password length 8-30 characters.</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "passwordcheck") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>Your passwords do not match!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "usertaken") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>This username is already taken!</p></a></div> \n";
-        }
-        else if ($_GET['error'] == "mailtaken") {
-            echo "<div id='formFooter'><a class='underlineHover'><p>This email is already taken!</p></a></div> \n";
-        }
-    }
-
-
-    ?>
-
 </div>
-
-
-
   <script src="js/scripts.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+    // Get the modal
+    var modal = document.getElementById("signupsuccsess-modal");
+    var mainBody = document.getElementById("signup-form");
+
+    function getParameter(parameterName) {
+        let parameters = new URLSearchParams(window.location.search);
+        return parameters.get(parameterName);
+    }
+
+    var signUpComplete =getParameter("success");
+
+    // Get the <span> element that closes the modal
+    var btn = document.getElementById("signupsuccsess-btn");
+
+    // When the user clicks on the button, open the modal
+    if (signUpComplete=="signupsuccess") {
+        modal.style.display = "block"
+        modal.style.opacity = 100
+        modal.style.position = "relative"
+        modal.style.top = "5%"
+        mainBody.style.display="none"
+    }
+    </script>
 </body>
 </html>
